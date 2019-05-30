afd094adce382892ae5314a08f29a4e183b08f75
Davinderpro01 <47469466+Davinderpro01@users.noreply.github.com>
2019-05-29 23:30:52 -0600

    mi primer commit

---
+++ neural.py
@@ -0,0 +1,9 @@
+print(" ")
+print("                                               HOLA Y BIENVENIDO A MI PROGRAMA")
+print(" ")
+a = int(input("ingrese un número = "))
+if a>=60:
+    print("usted es un ganador")
+else:
+    print("usted es un perdedor")
+    "# neuralproxus" 
